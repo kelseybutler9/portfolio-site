85385882b179ef75aaed254cd3a703466d85eaa5
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-11-25 12:56:31 -0500

    update styling for navigation

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(`li`).click(function() {
-  this.addClass('.selected');
+  let sectionOptions = ['#home', '#aboutme', '#projects', '#contactinfo'];
+  let sectionName = $(this).find('a').attr('href');
+  sectionOptions.forEach(item => {
+    $(`${item}`).prop("hidden", true);
+  });
+
+  $(`${sectionName}`).prop("hidden", false);
 });
